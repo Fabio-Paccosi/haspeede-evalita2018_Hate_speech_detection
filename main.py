4a9a01f37560478dc0ac858fd8eb5207b61ab422
Fabio Paccosi <info@smart-consult.it>
2022-05-29 10:41:04 +0200

    Code improvements and clean

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import os
 
 # CONSTANTI
 PROJECT_TITLE = "haspeede@evalita 2018 Project by Fabio Paccosi matr. 307616"
-VERSION_NUMBER = "0.1";
+VERSION_NUMBER = "0.1"
 EMBEDDING_DIM = 64
 # MAX_SEQUENCE_LENGTH = 500
 MAX_SEQUENCE_LENGTH = 408
@@ -35,7 +35,8 @@ MULTIPLE_CONV_LEVEL = True
 
 # Configurazioni Top-level
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' # Nascondo dalla console i messaggi che manda TensorFlow in cui ci avverte che la nostra esecuzione potrebbe essere più veloce utilizzano hardware specifico
-spacy.prefer_gpu() # Esegue le operazioni di spacy su GPU, se disponibile.
+spacy.prefer_gpu()# Esegue le operazioni di spacy su GPU, se disponibile.
+
 
 #
 def conv_net(max_sequence_length, num_words, embedding_dim, no_embedding_input, trainable=False):
@@ -71,6 +72,7 @@ def conv_net(max_sequence_length, num_words, embedding_dim, no_embedding_input, 
     model.summary()
     return model
 
+
 #
 def get_conv_model(max_sequence_length, size, embedding_dim, no_embedding_input):
     model = Sequential()
@@ -91,9 +93,10 @@ def get_conv_model(max_sequence_length, size, embedding_dim, no_embedding_input)
     model.summary()
     return model
 
+
 #
-def exec_ml(data, multiple_conv=True, no_embedding_input=False):
-    print('Start ml')
+def exec_ml(data, log_level, multiple_conv=True, no_embedding_input=False):
+    print('Inizio la procedura di Machine Learning...')
 
     x_train, x_test, y_train, y_test = data
 
@@ -104,53 +107,59 @@ def exec_ml(data, multiple_conv=True, no_embedding_input=False):
         x_train = np.expand_dims(x_train, axis=2)
         x_test = np.expand_dims(x_test, axis=2)
 
-    print('Train shape: '+str(x_train.shape))
-    print('Test shape: '+str(x_test.shape))
+    if log_level == 1:
+        print('Train shape: '+str(x_train.shape))
+        print('Test shape: '+str(x_test.shape))
 
-    #try:
-    model = conv_net(MAX_SEQUENCE_LENGTH, len(x_train) + 1, EMBEDDING_DIM, no_embedding_input,
-                         trainable=True) if multiple_conv else get_conv_model(MAX_SEQUENCE_LENGTH, len(x_train) + 1,
-                                                                              EMBEDDING_DIM, no_embedding_input)
+    try:
+        model = conv_net(MAX_SEQUENCE_LENGTH, len(x_train) + 1, EMBEDDING_DIM, no_embedding_input,
+                             trainable=True) if multiple_conv else get_conv_model(MAX_SEQUENCE_LENGTH, len(x_train) + 1,
+                                                                                  EMBEDDING_DIM, no_embedding_input)
 
-    x_array = np.array(x_train)
-    y_array = np.array(y_train)
-    history = model.fit(x_array, y_array, validation_split=0.25, epochs=EPOCHS, verbose=1)
+        x_array = np.array(x_train)
+        y_array = np.array(y_train)
+        history = model.fit(x_array, y_array, validation_split=0.25, epochs=EPOCHS, verbose=1)
 
-    plt.plot()
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
+        plt.plot()
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
 
-    plt.title('Model metrics')
-    plt.ylabel('Metrics')
-    plt.xlabel('Epoch')
-    plt.legend(['Train Acc', 'Test Acc', 'Loss Train', 'Loss Test'], loc='upper left')
-    plt.show()
+        plt.title('Model metrics')
+        plt.ylabel('Metrics')
+        plt.xlabel('Epoch')
+        plt.legend(['Train Acc', 'Test Acc', 'Loss Train', 'Loss Test'], loc='upper left')
+        plt.show()
 
-    print(model.metrics_names)
-    #print(model.evaluate(x_test, y_test))
-    print(model.evaluate(x_array, y_array))
-    #except Exception as e:
-    #    print('--- ML ERROR ---')
+        if log_level == 1:
+            print(model.metrics_names)
+            print(model.evaluate(x_array, y_array))
+    except Exception as e:
+        print('--- ML ERROR ---')
+        print(str(e))
 
-#
+
+# Metodo per il salvataggio del file .pkl contenente le features estratte
 def store_features(data, storage_name):
     with open('data/features/'+storage_name + '.pkl', 'wb') as f:
         pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
-#
+
+# Metodo per il caricamento del file .pkl contenente le features estratte
 def load_features(storage_name):
     with open('data/features/'+storage_name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
-#
+
+# Metodo che ritorna le matrici divise in set di addestramento e test casuali
 def to_data(data_matrix):
     X = list(map(lambda x: x['features'], data_matrix))
     y = list(map(lambda x: 0 if x['tag'].lower() == '0' else 1, data_matrix))
-    print('splitting....')
-    # Return splitted arrays or matrices into random train and test subsets.
+    print('- Eseguo lo la divisione dei dati...')
+    # Richiamo il metodo della libreria sklearn per dividere i set passando la dimensione desiderata del set di test
     return train_test_split(X, y, test_size=0.33)
+
 
 #
 def tree_height(root):
@@ -164,6 +173,7 @@ def tree_height(root):
         return 1
     else:
         return 1 + max(tree_height(x) for x in root.children)
+
 
 #
 def get_heights_measure(paragraph, nlp):
@@ -183,85 +193,9 @@ def get_heights_measure(paragraph, nlp):
     except Exception as e:
         return 0
 
+
 # La tokenizzazione suddivide il contenuto di una frase (o di un testo) in parole (o in frasi) chiamate appunto token.
 # La tokenizzazione aiuta a interpretare il significato del testo analizzando la sequenza delle parole.
-'''
-def tokenize_data(data):
-    # Installiamo il modulo desiderato con il comando 'spacy download [nome_pipeline]'
-    # Creiamo l'oggetto 'nlp' importando una tra le pipeline di spacy già addestrate per la lingua italiana.
-    nlp = spacy.load("it_core_news_sm") # Modello indicato se si vuole efficienza nella computazione (13 MB)
-    #nlp = spacy.load("it_core_news_md") # (43 MB)
-    #nlp = spacy.load("it_core_news_lg") # Modello indicato se si vuole accuratezza nella computazione (544 MB)
-
-    vector = []
-    counter_verb = 0
-    counter_adj = 0
-    counter_punct = 0
-    counter_stop = 0
-    counter_sym = 0
-
-    # Per ogni elemento in data, richiamiamo la funzione nlp
-    # L'elaborazione delle frasi con l'oggetto nlp restituisce un oggetto doc che contiene le informazioni sui token, le loro caratteristiche linguistiche e le loro relazioni.
-    for item in data:
-        print(item)
-        try:
-            tokens = nlp(item["post"])
-            for token in tokens:
-                print(token)
-                vector.append(token.vector_norm)
-                if not token.is_stop:
-                    counter_stop += 1
-                if token.is_punct:
-                    counter_punct += 1
-                if token.pos_ == 'ADJ':
-                    counter_adj += 1
-                if token.pos_ == 'VERB':
-                    counter_verb += 1
-                if token.pos_ == 'SYM':
-                    counter_sym += 1
-
-            max_of_vector = tokens.vector.max()
-            min_of_vector = tokens.vector.min()
-            avg_of_vector = np.mean(tokens.vector)
-            paragraph_height_max, paragraph_height_min, paragraph_height_avg = get_heights_measure(tokens, nlp)
-            size = len(vector)
-            features = [
-                size,
-                max_of_vector,
-                min_of_vector,
-                avg_of_vector,
-                # counter_punct,
-                counter_punct / size,
-                # counter_stop,
-                counter_stop / size,
-                # counter_verb,
-                counter_verb / size,
-                # counter_adj,
-                counter_adj / size,
-                # counter_sym,
-                counter_sym / size,
-                # paragraph_height_max,
-                paragraph_height_max / size,
-                # paragraph_height_min,
-                paragraph_height_min / size,
-                # paragraph_height_avg,
-                paragraph_height_avg / size,
-            ]
-            # print("Features:")
-            # print(features)
-            # print("Tokens vector:")
-            # print(tokens.vector)
-            # data.append(np.concatenate([features, tokens.vector]))
-            data['features'] = np.concatenate([features, tokens.vector])
-            # doc['features'] = features
-            # doc['features'] = tokens.vector
-            # return data
-        except Exception as e:
-            print(e)
-
-    return data
-'''
-
 def tokenizer_func(nlp):
     def inner(doc):
         vector = []
@@ -319,16 +253,22 @@ def tokenizer_func(nlp):
 
     return inner
 
+# La funzione carica un modello della libreria spacy nell'oggetto chiamato 'nlp' e richiama la funzione 'tokenizer_func' con quell'oggetto su ogni elemento/frase del dataset
+# I seguenti modelli di spacy, devono essere prima scaricati e installati con il comando 'spacy download [nome_modello]' per poter essere utilizzati
+def tokenize_data(data, model_level):
+    if model_level == 0:
+        nlp = spacy.load("it_core_news_sm")  # Modello indicato se si vuole efficienza nella computazione (13 MB)
+    elif model_level == 1:
+        nlp = spacy.load("it_core_news_md")  # (43 MB)
+    else:
+        nlp = spacy.load("it_core_news_lg")  # Modello indicato se si vuole accuratezza nella computazione (544 MB)
 
-def tokenize_data(data):
-    nlp = spacy.load("it_core_news_sm")  # Modello indicato se si vuole efficienza nella computazione (13 MB)
-    # nlp = spacy.load("it_core_news_md") # (43 MB)
-    # nlp = spacy.load("it_core_news_lg") # Modello indicato se si vuole accuratezza nella computazione (544 MB)
     return list(map(tokenizer_func(nlp), list(filter(lambda x: not x.get('post', None) is None, data))))
+
 
 # Utilizzando la librerie RE e il modulo stopwords di nltk, pulisco le frasi della tabella dai dati non necessari
 # Perchè farlo? Un post di FB o un Tweet potrebbe essere differente da un testo tradizionale: uso di caratteri speciali, parole abbreviate o non corrette etc.
-# L'obiettivo della funzione 'sentence_clean' è quello di rimuovere quindi i caratteri speciali, link e tutto quello che non aggiunge significato alla frase
+# L'obiettivo è quello di rimuovere quindi i caratteri speciali, link e tutto quello che non aggiunge significato alla frase
 def sentence_clean(sent):
     # Rimuovo i caratteri di punteggiatura e i numeri
     sentence = re.sub('[^a-zA-Z]', ' ', sent)
@@ -346,8 +286,9 @@ def sentence_clean(sent):
     #Ritorna la frase con i caratteri minuscoli
     return sentence.lower()
 
+
 # Effettuo il parsing dei dati e ritorno un array di coppie di valori [post, tag] dove post rappresenta il testo dell'utente e tag i valori di 1 o 0, se presente un sentimento di odio nel testo o meno
-def parse_data(file_path):
+def parse_data(file_path, log_level=0):
     try:
         rows = []
         # Utilizzo il separatore '\t' proprio del file .tsv
@@ -356,8 +297,8 @@ def parse_data(file_path):
                            engine='python',
                            error_bad_lines=False,
                            warn_bad_lines=False)
-
-        print("-- Training Set File infos:" % file.columns, file.shape, len(file))
+        if log_level == 1:
+            print("-- Training Set File infos:" % file.columns, file.shape, len(file))
 
         with open(file_path, "r", encoding ="utf8") as f:
             reader = csv.reader(f, delimiter="\t")
@@ -371,18 +312,29 @@ def parse_data(file_path):
     except Exception as e:
         print(e)
 
-def get_features():
+# Estraggo le features dai dataset desiderati e salvo il risultato nel file .pkl scelto
+def get_features(log_level, model_level):
     input_files = input(
         "Inserisci i path dei dataset (uno o più) che desidere analizzare, divisi dal carattere \' :\n=> ")
     features_storage_name = input("Inserisci il nome del file che contiene le features estratte:\n=> ")
     data = []
     paths = input_files.split(", ")  # Esempio di path: data/train/haspeede_FB-train.tsv
     for file_path in paths:
-        print('- Parsing data: ' + file_path)
+        print('-> Eseguo il parsing del set: ' + file_path)
         data = data + parse_data(file_path)
-    # print(data) # Vediamo i dati parsati nell'oggetto 'data'
-    data = tokenize_data(data)
-    # print(data)
+
+    # Vediamo i dati parsati nell'oggetto 'data'
+    if log_level == 1:
+        print(data)
+
+    # Tokenizziamo i dati dell'array
+    data = tokenize_data(data, model_level)
+
+    # Visualizziamo i dati tokenizzati
+    if log_level == 1:
+        print(data)
+
+    # Salvo il risultato dell'estrazione
     store_features(data, features_storage_name)
     return features_storage_name
 
@@ -396,23 +348,28 @@ def get_user_input():
 
     try:
         selected_option = int(input("Digita il numero di una tra le opzioni disponibili => "))
+        log_level = int(input("Digita il livello di log che voi attivare [0 = nessuno, 1 = attivo] => "))
         if (selected_option == 1):
-            loaded_features = load_features(get_features())
+            model_level = int(input("Digita il livello di accuratezza del modello della pipeline di addestramento [0 = efficiente, 1 = intemedio, 2 = accurato] => "))
+            loaded_features = load_features(get_features(log_level, model_level))
             dataset = to_data(loaded_features)
-            exec_ml(dataset, multiple_conv=MULTIPLE_CONV_LEVEL, no_embedding_input=NO_EMBEDDING)
+            exec_ml(dataset, log_level, multiple_conv=MULTIPLE_CONV_LEVEL, no_embedding_input=NO_EMBEDDING)
         elif (selected_option == 2):
-            feature_file = get_features()
+            model_level = int(input("Digita il livello di accuratezza del modello della pipeline di addestramento [0 = efficiente, 1 = intemedio, 2 = accurato] => "))
+            feature_file = get_features(log_level, model_level)
             print("I risultati dell\'estrazione delle features sono stati salvati nel file \'" + feature_file + ".pkl\'")
         elif (selected_option == 3):
             feature_file = input()
             loaded_features = load_features(feature_file)
             dataset = to_data(loaded_features)
-            exec_ml(dataset, multiple_conv=MULTIPLE_CONV_LEVEL, no_embedding_input=NO_EMBEDDING)
+            exec_ml(dataset, log_level, multiple_conv=MULTIPLE_CONV_LEVEL, no_embedding_input=NO_EMBEDDING)
         else:
             print("Scelta non corretta!\n")
             get_user_input()
     except Exception as e:
-        print("Errore! Devi inserire un carattere numerico...\n\n")
+        print("=========== ERRORE ===========")
+        print(str(e))
+        print("==============================")
         get_user_input()
 
     print("========================================")
